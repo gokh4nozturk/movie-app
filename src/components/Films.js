4cb40ab93509bd4689062c43cfe15fe074fc9c2e
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-06-17 12:18:10 +0300

    fetched datas

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,59 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function Films() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await Axios.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=8ebecc9f6798ef3e2aa77ea37765848b&language=en-US&page=1"
+    ).then((res) => res.data);
+    setPopular(data.results);
+    console.log(data.results);
+  };
+
   return (
     <div>
-      <h1>Weekly Top Rated Movies</h1>
-      <p>
-        Impedit eum sed delectus similique esse accusamus sunt. Aliquid et
-        accusamus totam quaerat ea harum. Iusto labore omnis soluta repudiandae
-        dignissimos omnis aut. Veniam unde laudantium nihil ullam qui adipisci.
-        Eum excepturi consequuntur iure. Repellendus totam vitae aut iste natus
-        aut accusantium asperiores. Facere qui sapiente sit velit ea ut. In
-        voluptas sed blanditiis. Voluptas sed magnam atque sunt accusantium ea
-        officiis assumenda. Numquam praesentium omnis nam odio voluptates iste
-        vitae minus. Dolorem aliquam omnis qui est ea. Repudiandae tenetur quia
-        voluptatem ex. Dignissimos iure dignissimos distinctio culpa vel
-        inventore. Blanditiis ullam nisi necessitatibus veritatis. Sint velit
-        pariatur deleniti. Repellendus dolor sit quam aut occaecati ad
-        perspiciatis sequi. Dignissimos deserunt illum et qui maxime. Impedit
-        eum sed delectus similique esse accusamus sunt. Aliquid et accusamus
-        totam quaerat ea harum. Iusto labore omnis soluta repudiandae
-        dignissimos omnis aut. Veniam unde laudantium nihil ullam qui adipisci.
-        Eum excepturi consequuntur iure. Repellendus totam vitae aut iste natus
-        aut accusantium asperiores. Facere qui sapiente sit velit ea ut. In
-        voluptas sed blanditiis. Voluptas sed magnam atque sunt accusantium ea
-        officiis assumenda. Numquam praesentium omnis nam odio voluptates iste
-        vitae minus. Dolorem aliquam omnis qui est ea. Repudiandae tenetur quia
-        voluptatem ex. Dignissimos iure dignissimos distinctio culpa vel
-        inventore. Blanditiis ullam nisi necessitatibus veritatis. Sint velit
-        pariatur deleniti. Repellendus dolor sit quam aut occaecati ad
-        perspiciatis sequi. Dignissimos deserunt illum et qui maxime.Impedit eum
-        sed delectus similique esse accusamus sunt. Aliquid et accusamus totam
-        quaerat ea harum. Iusto labore omnis soluta repudiandae dignissimos
-        omnis aut. Veniam unde laudantium nihil ullam qui adipisci. Eum
-        excepturi consequuntur iure. Repellendus totam vitae aut iste natus aut
-        accusantium asperiores. Facere qui sapiente sit velit ea ut. In voluptas
-        sed blanditiis. Voluptas sed magnam atque sunt accusantium ea officiis
-        assumenda. Numquam praesentium omnis nam odio voluptates iste vitae
-        minus. Dolorem aliquam omnis qui est ea. Repudiandae tenetur quia
-        voluptatem ex. Dignissimos iure dignissimos distinctio culpa vel
-        inventore. Blanditiis ullam nisi necessitatibus veritatis. Sint velit
-        pariatur deleniti. Repellendus dolor sit quam aut occaecati ad
-        perspiciatis sequi. Dignissimos deserunt illum et qui maxime.Impedit eum
-        sed delectus similique esse accusamus sunt. Aliquid et accusamus totam
-        quaerat ea harum. Iusto labore omnis soluta repudiandae dignissimos
-        omnis aut. Veniam unde laudantium nihil ullam qui adipisci. Eum
-        excepturi consequuntur iure. Repellendus totam vitae aut iste natus aut
-        accusantium asperiores. Facere qui sapiente sit velit ea ut. In voluptas
-        sed blanditiis. Voluptas sed magnam atque sunt accusantium ea officiis
-        assumenda. Numquam praesentium omnis nam odio voluptates iste vitae
-        minus. Dolorem aliquam omnis qui est ea. Repudiandae tenetur quia
-        voluptatem ex. Dignissimos iure dignissimos distinctio culpa vel
-        inventore. Blanditiis ullam nisi necessitatibus veritatis. Sint velit
-        pariatur deleniti. Repellendus dolor sit quam aut occaecati ad
-        perspiciatis sequi. Dignissimos deserunt illum et qui maxime.
-      </p>
+      <h1>Popular</h1>
+      <div className="films-popular">
+        {popular.map((item) => {
+          return (
+            <div key={item.id} className="card">
+              <img
+                alt="asdfaf"
+                src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
+              ></img>
+              <div className="card-body">
+                <p className="card-text">{item.title}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/SidebarLeft.js
+++ src/components/SidebarLeft.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Axios from "axios";
 
 function SidebarLeft() {
+  const [genres, setGenres] = useState([]);
+  useEffect(() => {
+    fetchGenres();
+  }, []);
+  const fetchGenres = async () => {
+    const data = await Axios.get(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=8ebecc9f6798ef3e2aa77ea37765848b&language=en-US"
+    ).then((res) => res.data);
+    setGenres(data.genres);
+  };
+
   return (
     <div>
-      <h3>Kategoriler</h3>
-      <ul>
-        <li>Kategori 1</li>
-        <li>Kategori 2</li>
-        <li>Kategori 3</li>
-        <li>Kategori 4</li>
-        <li>Kategori 5</li>
-        <li>Kategori 6</li>
-        <li>Kategori 7</li>
-        <li>Kategori 8</li>
-      </ul>
+      <h3>Genres</h3>
+      {genres.map((item) => {
+        return <p key={item.id}>{item.name}</p>;
+      })}
     </div>
   );
 }
