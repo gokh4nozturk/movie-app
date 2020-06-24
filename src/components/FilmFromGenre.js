d5aedf063881872318e64698a3ab0a36b3cd62df
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-06-24 13:04:53 +0300

    overall finished

--- src/components/FilmFromGenre.js
+++ src/components/FilmFromGenre.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Axios from "axios";
 
 import SidebarLeft from "./SidebarLeft";
+import Film from "./Film";
 
-function FilmFromGenre() {
+function FilmFromGenre({ match }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchFromGenres();
+  }, []);
+
+  const fetchFromGenres = async () => {
+    const data = await Axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=8ebecc9f6798ef3e2aa77ea37765848b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${match.params.id}`
+    ).then((res) => res.data);
+    setData(data.results);
+  };
+
   return (
     <div className="home-main">
       <div className="sidebar-left">
         <SidebarLeft />
       </div>
-      <div className="view">Buraya türe göre filmler gelecek</div>
+      <div className="view">
+        <div className="view-from-genres">
+          {data.map((item) => (
+            <Film key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
