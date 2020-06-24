59385a4679cf83cd8f83b9cd03b4c0bf1f484751
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-06-24 14:03:34 +0300

    improvements

--- src/components/TvShowDetail.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function TvShowDetail() {
-  return <div>Tv Show Detail</div>;
-}
-export default TvShowDetail;
--- src/components/TvShows.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function TvShows() {
-  return <div>Tv Shows</div>;
-}
-export default TvShows;
