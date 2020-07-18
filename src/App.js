e1eb02d33c9d2620e9bf411954733c7a9027795b
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-07-18 18:29:48 +0300

    improvements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
